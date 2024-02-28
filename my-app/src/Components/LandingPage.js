0869a0558bf9fa6fd03527e9b6f2197fd8c6e68a
ColinO'Brien <colinpobrien04@gmail.com>
2024-02-27 18:32:38 -0800

    First draft of landing page text

---
+++ my-app/src/Components/LandingPage.js
@@ -0,0 +1,17 @@
+import './LandingPage.css';
+
+function LandingPage() {
+    return (
+        <div className="landers">
+            <div className="name">Colin O'Brien</div>
+            <div className="tinyLanders">
+                <div className="landed">About Me</div>
+                <div className="landed">Experience</div>
+                <div className="landed">Projects</div>
+                <div className="landed">Contact</div>
+            </div>
+        </div>
+    )
+}
+
+export default LandingPage;
