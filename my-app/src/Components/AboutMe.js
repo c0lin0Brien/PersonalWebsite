d6ce69e8253ed0186492fd29fcd16d11334d5f5d
ColinO'Brien <colinpobrien04@gmail.com>
2024-02-27 19:40:34 -0800

    First implementation of About Me page

---
+++ my-app/src/Components/AboutMe.js
@@ -0,0 +1,18 @@
+import './AboutMe.css';
+
+function AboutMe() {
+    return (
+        <div className="whatAboutMe">
+            <div classname="abouts">
+                <div>About Me</div>
+            </div>
+            <div className="body">
+                Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Et malesuada fames ac turpis egestas sed tempus. Nunc sed augue lacus viverra vitae congue eu consequat. Massa sapien faucibus et molestie ac feugiat sed. Mauris cursus mattis molestie a iaculis at erat pellentesque adipiscing. Ut enim blandit volutpat maecenas volutpat blandit aliquam etiam. Volutpat commodo sed egestas egestas. Vestibulum sed arcu non odio. Euismod elementum nisi quis eleifend quam adipiscing. At consectetur lorem donec massa. Semper eget duis at tellus at urna condimentum. Congue eu consequat ac felis donec et odio pellentesque. Mi quis hendrerit dolor magna eget est lorem ipsum dolor. Donec et odio pellentesque diam volutpat. Orci a scelerisque purus semper eget. Eu sem integer vitae justo eget magna fermentum iaculis eu. Varius morbi enim nunc faucibus a. Facilisis leo vel fringilla est ullamcorper eget. Tincidunt eget nullam non nisi est sit amet.
+                <br />
+                Phasellus vestibulum lorem sed risus ultricies tristique nulla aliquet. Id diam vel quam elementum pulvinar etiam non quam. At elementum eu facilisis sed. Sit amet est placerat in egestas. Est ullamcorper eget nulla facilisi etiam dignissim diam quis enim. Urna porttitor rhoncus dolor purus. Egestas integer eget aliquet nibh praesent tristique magna sit. Commodo elit at imperdiet dui accumsan sit amet nulla. Vel turpis nunc eget lorem dolor sed viverra ipsum nunc. Felis bibendum ut tristique et egestas. Lorem ipsum dolor sit amet consectetur adipiscing elit duis tristique. Egestas sed sed risus pretium. Scelerisque fermentum dui faucibus in.
+            </div>
+        </div>
+    )
+}
+
+export default AboutMe;
