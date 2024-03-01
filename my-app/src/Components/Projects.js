1bffb40c0bbb45de81c17dc9396d6053a152297c
ColinO'Brien <colinpobrien04@gmail.com>
2024-03-01 11:13:10 -0800

    Projects section

---
+++ my-app/src/Components/Projects.js
@@ -0,0 +1,13 @@
+import { ResumeBlock } from "./ResumeBlocks";
+import './ResumeBlocks.css';
+
+function Projects() {
+    return (
+        <div className='header'>
+            Projects
+            <ResumeBlock job={"COMING SOON"} description={"BIG THINGS COMING SOON"} />
+        </div>
+    )
+}
+
+export default Projects;
